c4930b9c1499a402d3706939099362815cf436db
Aleksei Tsikov <alexey.tsikov@gmail.com>
2020-10-17 11:18:53 +0100

    Remove locked transitive dependency version

--- common/config/rush/pnpmfile.js
+++ common/config/rush/pnpmfile.js
@@ -1,14 +1,14 @@
 function readPackage(packageJson, context) {
-  if (packageJson.name === '@storybook/react') {
-    context.log('Lock webpack dependency to reproduce update error')
-    return {
-      ...packageJson,
-      dependencies: {
-        ...packageJson.dependencies,
-        webpack: '4.44.1',
-      },
-    }
-  }
+  // if (packageJson.name === '@storybook/react') {
+  //   context.log('Lock webpack dependency to reproduce update error')
+  //   return {
+  //     ...packageJson,
+  //     dependencies: {
+  //       ...packageJson.dependencies,
+  //       webpack: '4.44.1',
+  //     },
+  //   }
+  // }
 
   return packageJson
 }
