d6bc244c569c05fe37cb583a77823db8f6823eab
Aleksei Tsikov <alexey.tsikov@gmail.com>
2020-10-17 11:16:40 +0100

    Use pnpmfile instead of common-versions to install outdated dependency

---
+++ common/config/rush/pnpmfile.js
@@ -0,0 +1,20 @@
+function readPackage(packageJson, context) {
+  if (packageJson.name === '@storybook/react') {
+    context.log('Lock webpack dependency to reproduce update error')
+    return {
+      ...packageJson,
+      dependencies: {
+        ...packageJson.dependencies,
+        webpack: '4.44.1',
+      },
+    }
+  }
+
+  return packageJson
+}
+
+module.exports = {
+  hooks: {
+    readPackage,
+  },
+}
