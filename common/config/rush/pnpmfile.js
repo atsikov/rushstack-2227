b6ededde15b80ec4ed7c9449b3ccc3df6d6ca1a3
Aleksei Tsikov <alexey.tsikov@gmail.com>
2020-12-20 15:17:48 +0000

    Update pnpm and rush to latest versions

--- common/config/rush/pnpmfile.js
+++ common/config/rush/pnpmfile.js
@@ -1,20 +1,23 @@
 function readPackage(packageJson, context) {
-  if (packageJson.name === '@storybook/react') {
-    context.log('Lock webpack dependency to reproduce update error')
+  if (
+    packageJson.name === "@storybook/react" &&
+    "webpack" in packageJson.dependencies
+  ) {
+    context.log("Lock webpack dependency to reproduce update error");
     return {
       ...packageJson,
       dependencies: {
         ...packageJson.dependencies,
-        webpack: '4.44.1',
+        webpack: "4.44.1",
       },
-    }
+    };
   }
 
-  return packageJson
+  return packageJson;
 }
 
 module.exports = {
   hooks: {
     readPackage,
   },
-}
+};
